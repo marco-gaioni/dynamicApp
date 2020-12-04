c01f4e49a0640d64801a37ddfa4639f343148e61
marco-gaioni <62023472+marco-gaioni@users.noreply.github.com>
2020-12-04 17:18:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require('express')
 const plumbus = require('rickmortyapi')
-const app = express();
+const app = new express()
 const port = 8080;
-const handlebars = require('express-handlebars');
+const handlebars = require('express-handlebars')
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs')
 
 app.engine('hbs', handlebars({
     layoutsDir: __dirname + '/views/layouts',
     extname: 'hbs',
     defaultLayout: 'planB',
     partialsDir: __dirname + '/views/partials/'
-}));
+}))
 
 app.use(express.static('public'))
 
 app.get('/', async (req, res) => {
   res.render('intro', {layout: 'index', nome : req.query.nome, cognome : req.query.cognome, eta : req.query.eta, country : req.headers["accept-language"].substr(0,2), saluto : "Hello man"})
-  
-});
+})
 
 app.get('/characters', async (req, res) => {
   const chars = (await plumbus.getCharacter({
@@ -27,10 +26,10 @@ app.get('/characters', async (req, res) => {
     species : req.query.species
   }))
   if(req.headers["accept-language"].substr(0,2)==="it"){
-    res.render('main', {layout: 'index', chars: chars.results, saluto : "Ciao uomo! ecco i personaggi squanchosi!"});
+    res.render('main', {layout: 'index', chars: chars.results, saluto : "Ciao uomo! ecco i personaggi squanchosi!"})
   }else{
     res.render('main', {layout: 'index', chars: chars.results, saluto : "Hello man! here your squancheos characters!"})
   }
-});
+})
 
-app.listen(port, () => console.log(`App listening to port ${port}`));
+app.listen(port, () => console.log(`App listening to port ${port}`))
